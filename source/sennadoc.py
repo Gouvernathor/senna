815abd03b05cf9ad001c597976187efb3d3e6bf3
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2022-11-28 02:01:44 +0100

    réimplémentation partielle

--- source/sennadoc.py
+++ source/sennadoc.py
@@ -1,63 +1,82 @@
 from collections import defaultdict
-import warnings
 import docutils.nodes
-import docutils.transforms
-from docutils.parsers.rst import Directive
+import sphinx.addnodes
+from sphinx.errors import ExtensionError
+import sphinx.roles
+import sphinx.domains
+import sphinx.domains.std
+import sphinx.util.docutils
 
 counters = defaultdict(int)
 registered_articles = {}
 
-class ArticleDirective(Directive):
-    required_arguments = 1
-    final_argument_whitespace = True
-
+class ArticleDirective(sphinx.domains.std.Target):
+    """
+    Piggyback Target and recompute fullname (but not node_id)
+    """
     def run(self):
-        name, = self.arguments
-        id = name.lower().replace("'", "").replace(" ", "-")
+        node, = super().run()
+        node_id, *_ = node["ids"]
+        # recompute fullname - Target doesn't save it properly
+        fullname = sphinx.domains.std.ws_re.sub(' ', self.arguments[0].strip())
+
         docname = self.state.document.current_source
-
         counters[docname] += 1
-        if id in registered_articles:
-            warnings.warn(f"Article {id} registered twice")
-        registered_articles[id] = counters[docname]
-        name = f"Article {counters[docname]} - {name}"
-        id2 = f"article-{counters[docname]}"
+        if fullname in registered_articles:
+            raise ExtensionError(f"an article {fullname!r} is already registered")
+        registered_articles[fullname] = counters[docname]
+        name = f"Article {counters[docname]} - {fullname}"
+        # yes, the name is longer than the fullname
 
-        return [docutils.nodes.section(id,
-                                       docutils.nodes.title(id, name),
-                                       ids=[id, id2])]
+        return [node,
+                docutils.nodes.section(name,
+                                       docutils.nodes.title(node_id, name),
+                                       ids=[node_id]),
+                ]
 
-delayed = object()
-delayedshort = object()
+artrefflag = object()
+artnumrefflag = object()
 
-def art_role(name, rawtext, text, lineno, inliner, options=None, content=None):
-    if name == "artref":
-        sentinel = delayed
-    elif name == "artnumref":
-        sentinel = delayedshort
-    nod = docutils.nodes.reference(text, None, refid=text)
-    nod.flag = sentinel
-    return [nod], []
+class ArtRole(sphinx.roles.XRefRole):
+    innernodeclass = docutils.nodes.generated
 
-class SennaTransform(docutils.transforms.Transform):
+    def __init__(self, *args, flag, **kwargs):
+        self.flag = flag
+        super().__init__(*args, **kwargs)
+
+    def result_nodes(self, *args, **kwargs):
+        (node,), msgs = super().result_nodes(*args, **kwargs)
+        node.flag = self.flag
+        return [node], msgs
+
+class ArtrefTransform(docutils.transforms.Transform):
     default_priority = 750
 
     def apply(self):
-        for node in self.document.traverse(docutils.nodes.reference):
+        for node in self.document.traverse(sphinx.addnodes.pending_xref):
+            if node["refexplicit"]:
+                continue
+            # print(node)
             flag = getattr(node, "flag", None)
+            # print(flag)
             if flag is not None:
-                if flag is delayed:
-                    st = "l'article {}"
-                elif flag is delayedshort:
-                    st = "{}"
-                rf = registered_articles.get(node["refid"], None)
+                rf = registered_articles.get(node["reftarget"], None)
+                # print(rf)
                 if rf is None:
-                    warnings.warn(f"Article {node['refid']!r} not found, a faulty artref was issued.")
-                node.children[:] = [docutils.nodes.Text(st.format(rf))]
+                    print(f"number of article {node['reftarget']!r} not found")
+                if flag is artnumrefflag:
+                    name = str(rf)
+                elif flag is artrefflag:
+                    name = f"l'article {rf}"
+                else:
+                    raise ExtensionError(f"unknown flag {flag!r}")
+                node.children[:] = [docutils.nodes.Text(name)]
 
 def setup(app):
-    app.add_directive("article", cls=ArticleDirective)
-    app.add_role("artref", art_role)
-    app.add_role("artnumref", art_role)
+    app.add_directive_to_domain("std", "article", ArticleDirective)
+    app.add_role_to_domain("std", "artref", ArtRole(flag=artrefflag))
+    app.add_role_to_domain("std", "artnumref", ArtRole(flag=artnumrefflag))
+    object_types = app.registry.domain_object_types.setdefault('std', {})
+    object_types["article"] = sphinx.domains.ObjType("article", "artref", "artnumref")
 
-    app.add_transform(SennaTransform)
+    app.add_transform(ArtrefTransform)
