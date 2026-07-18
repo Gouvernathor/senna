2bf37fc39f59ed11fbc1bc0d33521ca173a01352
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-07-18 11:11:38 +0200

    add visualizer

---
+++ graphing/src/browser.ts
@@ -0,0 +1,9 @@
+import cytoscape from "cytoscape";
+import data from "./data.js";
+import { convertData, styleOptions } from "./convertData.js";
+
+cytoscape({
+    container: document.getElementById("cy"),
+    elements: convertData(data),
+    style: styleOptions,
+});
