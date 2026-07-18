63c1aa66eb8cf0b001668368145358963c1e0317
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-07-18 10:06:05 +0200

    more specific

--- graphing/convertData.ts
+++ graphing/convertData.ts
@@ -1,26 +1,42 @@
-import { ElementsDefinition } from "cytoscape";
+import { ElementsDefinition, StylesheetJson } from "cytoscape";
 import { DataRoot } from "./data.js";
 
 export function convertData(data: DataRoot): ElementsDefinition {
     return {
         nodes: Object.keys(data.situations).map(id => ({ id, data: data.situations[id] })),
         edges: data["événements"].flatMap(({ n, name, desc, variantes, optional }) => {
-            const data = { name, desc };
             return variantes.flatMap(({ id, situations = [], condition, "résultat": resultat, nomination }) => {
+                const classes: string[] = [];
+                if (optional) {
+                    classes.push("optional");
+                } else if (nomination) {
+                    classes.push("nomination");
+                }
                 return situations.map(situation => ({
                     data: {
                         id: id ? `${n}-${id}` : `${n}`,
                         source: situation,
                         target: resultat,
 
-                        // nothing is done with this yet
-                        condition,
+                        name,
                         nomination,
-                        ...data,
                         optional,
+                        // nothing is done with these yet
+                        condition,
+                        desc,
                     },
+                    classes,
                 }));
             });
         }),
     };
 }
+
+export const styleOptions: StylesheetJson = [
+    {
+        selector: "[name]",
+        style: {
+            label: "data(name)",
+        },
+    }
+];
