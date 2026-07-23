bb86783699f425c96643ecf13f7986de8fedb093
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-07-23 15:48:25 +0200

    tentatively add subgraphs

--- graphing/src/graphviz/add-data.ts
+++ graphing/src/graphviz/add-data.ts
@@ -5,16 +5,29 @@ export function addData(graph: Graph, data: DataRoot) {
     addNodes(graph, data.situations);
     addEdges(graph, data["événements"]);
 }
+
 function addNodes(graph: Graph, situations: Situations) {
+    const parentNodeIds = Object.keys(situations).map(sId => situations[sId]?.parent).filter(sId => sId != undefined);
+
+    for (const parentId of parentNodeIds) {
+        const { name, desc } = situations[parentId]!;
+        using _ = graph.addSubgraph(parentId, {
+            label: name,
+            tooltip: desc,
+        });
+    }
+
     for (const sId of Object.keys(situations)) {
-        const { name, desc, parent } = situations[sId]!;
-        graph.addNode(sId, {
+        const { name, desc, parent: parentId } = situations[sId]!;
+        using parent = parentId ? graph.getSubgraph(parentId) : undefined;
+        (parent ?? graph).addNode(sId, {
             label: name,
             comment: desc,
             tooltip: desc,
         });
     }
 }
+
 function addEdges(graph: Graph, evenements: readonly Evenement[]) {
     for (const { n, name, desc, variantes, optional } of evenements) {
         for (const { id, name: varianteName, situations = [], /*condition,*/ "résultat": resultat, nomination } of variantes) {
